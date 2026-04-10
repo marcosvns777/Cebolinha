7b9dcb4dfd526135b5b057105d7f07826ddcb95d
Marcos <marcosviniciuus0103@gmail.com>
2026-04-10 20:11:36 -0300

    atividade cebolinha

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿string cebolinha = "Eu troco as letras, não falo bem, mas meus amigos não troco por ninguém";
+string trocaletras = cebolinha.Replace("r","l");
+
+Console.WriteLine($" {trocaletras}");
